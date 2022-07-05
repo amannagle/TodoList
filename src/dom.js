591c2bf26eac9eeb523c4b12f1c6c297586084d0
aman <aman.nagle1999@gmail.com>
2022-07-05 21:33:21 +0530

    added addtodo logic

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,8 @@
 import { todo } from "./todos";
 import { project } from "./project";
 
+const default_project = new project('Inbox',[]);
+let current_project = default_project;
 function interactWithDom()
 {
     addListenersToElements();
@@ -9,8 +11,37 @@ function interactWithDom()
 function addListenersToElements()
 {
     addListenerToProjectCreate();
+    addListenerToAddTasksDiv();
+    addListenerToAddTasksAndCancel();
+}
+function addListenerToAddTasksDiv()
+{
+    const addtasksbutton = document.querySelector('#add-task-div-button');
+    addtasksbutton.addEventListener('click',function(){
+        document.querySelector('.task-buttons').style['display']='block';
+        addtasksbutton.style['display']='none';
+    })
+}
+function addListenerToAddTasksAndCancel()
+{
+    document.querySelector('#add-task').addEventListener('click',addTask)
+    document.querySelector('#cancel').addEventListener('click',cancelTask)
+}
+function addTask(e)
+{
+    e.preventDefault();
+    const task_name = document.querySelector('#add-task-input').value;
+    const new_task = new todo(current_project,task_name);
+    current_project.addTodo(new_task);
 }
 
+function cancelTask()
+{
+    const addtasksbutton = document.querySelector('#add-task-div-button');
+    document.querySelector('.task-buttons').style['display']='none';
+    addtasksbutton.style['display']='block';
+
+}
 function addListenerToProjectCreate()
 {
     const addProjectButton = document.querySelector('#add-project');
@@ -25,7 +56,10 @@ function addListenerToProjectCreate()
         document.querySelector('#add-project-form').reset();
     })
 }
+function render_tasks()
+{
 
+}
 function render_project()
 {
    const length = project.allprojects.length;
